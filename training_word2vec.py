cf94147c2f78485d7d80421505ceed013cdeadbb
Arun Ravi <arunravi@Aruns-MBP.lan>
2021-11-26 19:27:27 -0600

    Fixed tensor bugs with input

--- training_word2vec.py
+++ training_word2vec.py
@@ -4,11 +4,18 @@ import pandas as pd
 import math
 import random
 from tqdm import tqdm
+import pickle
 
-from preprocessing_word2vec import VOCABULARY, VOCABULARY_SIZE, tokens, train_data, UNIGRAM_RATIOS, generateOneHotEncoding
+WINDOW_SIZE = 5             # WINDOW_SIZE number of words on either side of the input word
+VOCABULARY_SIZE = 0
+CORPUS_SIZE = 0
+
+VOCABULARY = {}
+UNIGRAM_RATIOS = []
+BERNOULLI_MAP = []          # Bernoulli Map to help with subsampling
 
 #hidden dimensionality of the word embeddings
-N = 300
+N = 50
 
 
 #Context Size
@@ -18,16 +25,51 @@ C = 5
 #VOCABULARY = {}
 
 #Entire Corpus Stored Here
-TOKENS = tokens
 
 #Learning Rate for SGD
 alpha = 0.001
 
 #No. of Negative Samples
-K = 5
+K = 2
 
 #Number of Epochs
 num_epochs = 3
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+"""
+Importing Vocabulary from pre-processing.py
+"""
+
+vocabulary_file = open("Pickle/vocabulary_file.pkl",'rb')
+VOCABULARY = pickle.load(vocabulary_file)
+vocabulary_file.close()
+
+
+VOCABULARY_SIZE = len(VOCABULARY)
+
+"""
+Importing Tokenized Corpus from pre-processing.py
+"""
+
+token_vocabulary_file = open("Pickle/token_vocabulary_file.pkl",'rb')
+tokens_vocab = pickle.load(token_vocabulary_file)
+token_vocabulary_file.close()
+
+"""
+Importing Training Data from pre-processing.py
+"""
+train_data_file = open("Pickle/train_data_file.pkl",'rb')
+train_data = pickle.load(train_data_file)
+train_data_file.close()
+
+"""
+Importing Unigram Ratios from pre-processing.py
+"""
+unigram_file = open("Pickle/unigram_ratios_file.pkl",'rb')
+UNIGRAM_RATIOS = pickle.load(unigram_file)
+unigram_file.close()
 
 """
 Function to generate Noise Distribution (Needs to be executed exactly once!)
@@ -36,16 +78,19 @@ Output : Noise distribution for Negative Sampling
 """
 def generateNoiseDist():
     #Read this variable from a txt file here
-    unigram_counts = [ele*len(TOKENS) for ele in UNIGRAM_RATIOS]
+    unigram_counts = len(tokens_vocab)*UNIGRAM_RATIOS
 
-    unigram_counts = [ele ** 0.75 for ele in unigram_counts]
+    unigram_counts = unigram_counts ** 0.75
     sum_counts = sum(unigram_counts)
 
-    noise_distribution = [ele / sum_counts for ele in unigram_counts]
+    noise_distribution = unigram_counts/sum_counts
 
     return noise_distribution
 
-
+def generateOneHotEncoding(word):
+    one_hot_encoded_vector = torch.zeros(VOCABULARY_SIZE)
+    one_hot_encoded_vector[VOCABULARY[word]] = 1
+    return one_hot_encoded_vector
 
 
 """Class implementing the Skip-Gram Model from scratch"""
@@ -55,17 +100,16 @@ class SkipGram():
     Constructor initializes input and output weight matrices
     """
     def __init__(self):
-        self.W_in = (-2/math.sqrt(N))*torch.rand(VOCABULARY_SIZE,N) + 1/math.sqrt(N)
-        self.W_out = (-2/math.sqrt(N))*torch.rand(N,VOCABULARY_SIZE) + 1/math.sqrt(N)
+        self.W_in = ((-2/math.sqrt(N))*torch.rand(VOCABULARY_SIZE,N) + 1/math.sqrt(N)).to(device)
+        self.W_out = ((-2/math.sqrt(N))*torch.rand(N,VOCABULARY_SIZE) + 1/math.sqrt(N)).to(device)
 
     """
     Function to perform a forward pass through the Skip Gram
     Input : Current Instance of Class
     Output : C probability distributions, each representing a context
     """
-    def forward(self, input_vector):
-        self.input_vector = input_vector
-        self.h = torch.matmul(torch.transpose(self.W_in, 0, 1), self.input_vector)
+    def forward(self, inp_index):
+        self.h = self.W_in[inp_index]
         self.out = torch.matmul(torch.transpose(self.W_out, 0 ,1), self.h)
         return self.out
 
@@ -74,15 +118,15 @@ class SkipGram():
     Input : Noise Distribution, input string, one hot encoded input, context string
     Output : All the gradients
     """
-    def gradients(self, noise_distribution, input, input_encoded, context):
+    def gradients(self, noise_distribution, inp_index, input_encoded, context):
 
-        self.inp_grad = torch.zeros(N)
+        self.inp_grad = torch.zeros(N).to(device)
         
         for id in range(len(context)):
 
             #performing the forward pass through the Skip Gram Network
 
-            self.forward(input_encoded)
+            self.forward(inp_index)
 
             #sampling K negative samples from the Noise Distribution
         
@@ -95,27 +139,23 @@ class SkipGram():
                 
             #updating output vector of the positive sample (our context word)
 
-            self.W_out[:,VOCABULARY[context[id]]] -= alpha * (torch.sigmoid(torch.dot(self.W_out[:,VOCABULARY[context[id]]], self.h)) - 1)*self.h
+            self.W_out[:,context[id]] -= alpha * (torch.sigmoid(torch.dot(self.W_out[:,context[id]], self.h)) - 1)*self.h
 
             #computing gradient for input vector
 
             for i in range(K):
                 self.inp_grad += torch.sigmoid(torch.dot(self.W_out[:,VOCABULARY[D_dash[i]]], self.h))* self.W_out[:,VOCABULARY[D_dash[i]]]
             
-            self.inp_grad += (torch.sigmoid(torch.dot(self.W_out[:,VOCABULARY[context[id]]], self.h)) - 1)*self.W_out[:,VOCABULARY[context[id]]] 
+            self.inp_grad += (torch.sigmoid(torch.dot(self.W_out[:,context[id]], self.h)) - 1)*self.W_out[:,context[id]] 
 
         #updating the input vector after all contexts are done
-        self.W_in[VOCABULARY[input]] -= alpha * self.inp_grad
-
-
+        self.W_in[inp_index] -= alpha * self.inp_grad
 
 
 """
+Format of each training instance
 (input_word, list containing all context words in the window)
 """
-training_data = train_data
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 #Generates the special unigram frequencies from which to negative sample from
 noise_distribution = generateNoiseDist()
@@ -123,19 +163,20 @@ noise_distribution = generateNoiseDist()
 skip_gram = SkipGram()
 
 
-for epoch in tqdm(range(num_epochs)):
-    for idx, (input, context) in enumerate(training_data):
 
-        input_encoded = generateOneHotEncoding(input).to(device)
-        
+
+for epoch in range(num_epochs):
+    for idx, (inp, context) in enumerate(tqdm(train_data)):
+
+        # print(inp)
+        input_encoded = generateOneHotEncoding(inp).to(device)
+        inp_index = VOCABULARY[inp]
+        # print(inp_index)
+
+        context = torch.tensor([VOCABULARY[c] for c in context]).to(device)
+
         #context = generateOneHotEncoding(context).to(device)
 
         #skip_gram.forward(input_encoded)
 
-        skip_gram.gradients(noise_distribution, input, input_encoded, context)
-
-print(list(VOCABULARY.keys())[0])
-
-print(skip_gram.W_in[0])
-
-print(skip_gram.W_in[0].shape)
+        skip_gram.gradients(noise_distribution, inp_index, input_encoded, context)
